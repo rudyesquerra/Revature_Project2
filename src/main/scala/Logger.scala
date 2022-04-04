f70407c86d79502f535add7f3e3fc056ec57979e
Youngjungkim016 <youngjung016@revature.net>
2022-04-04 10:45:47 -0400

    Create Logger.scala

---
+++ src/main/scala/Logger.scala
@@ -0,0 +1,20 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+object Logger {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+    Logger.getLogger("org.spark-project").setLevel(Level.ERROR)
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    System.setProperty("hadoop.home.dir", "C:/hadoop")
+    val spark = SparkSession
+      .builder
+      .appName("hello hive")
+      .config("spark.master", "local")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
+    spark.sparkContext.setLogLevel("ERROR")
+  }
+
+}
